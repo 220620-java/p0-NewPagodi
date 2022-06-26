24e134b49e2af9269d0d7dcb35dc16ad3325d0c2
New Pagodi <NewPagodi@users.noreply.github.com>
2022-06-26 01:40:02 -0500

    Ensure maven is running tests with small Demo Class

---
+++ src/main/java/com/thaleswell/mb/Demo.java
@@ -0,0 +1,11 @@
+package com.thaleswell.mb;
+
+public class Demo {
+    public boolean getBool() {
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
