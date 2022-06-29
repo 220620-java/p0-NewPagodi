636f58f95e8d240532ca60f1d7d5e4d6e6c49397
New Pagodi <NewPagodi@users.noreply.github.com>
2022-06-28 23:02:32 -0500

    Connect to local postgres database
    Eventually it will be necessary to a remote database on AWS,
    but for now simply connect to a local database to begin the
    development process.
    
    This requires first adding a small, mostly empty Main class
    to make the connection with.

--- src/main/java/com/thaleswell/mb/Demo.java
+++ src/main/java/com/thaleswell/mb/Demo.java
@@ -2,10 +2,6 @@ package com.thaleswell.mb;
 
 public class Demo {
     public boolean getBool() {
-        return false;
-    }
-    
-    public static void main(String[] args) {
-        System.out.println("test");
+        return true;
     }
 }
