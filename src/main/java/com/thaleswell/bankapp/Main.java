5f6acd2853260440788540329d32d30102516b6b
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-03 16:40:30 -0500

    Add banking service to simulate bank actions.

--- src/main/java/com/thaleswell/bankapp/Main.java
+++ src/main/java/com/thaleswell/bankapp/Main.java
@@ -1,6 +1,9 @@
 package com.thaleswell.bankapp;
 
 import java.util.Scanner;
+
+import com.thaleswell.bankapp.services.banking.DummyBankService;
+import com.thaleswell.bankapp.states.BankAppState;
 import com.thaleswell.bankapp.states.StartState;
 import com.thaleswell.tui.TUI;
 import com.thaleswell.tui.io.ConsoleIO;
@@ -8,14 +11,21 @@ import com.thaleswell.tui.io.IIO;
 
 public class Main {
     public static void main(String args[]) {
+        // Create a scanner and use it to create a consoleIO for out
+        // TUI states.
         final Scanner scanner = new Scanner(System.in);
         IIO consoleIO = new ConsoleIO(scanner);
 
-        StartState start = new StartState(consoleIO);
+        // Create a DummyBankService for our TUI states.
+        DummyBankService bankServices = new DummyBankService(consoleIO);
+        BankAppState.setBankService(bankServices);
 
+        // Create a new start state and run the TUI loop.
+        StartState start = new StartState(consoleIO);
         TUI ui = new TUI(consoleIO, start);
         ui.runLoop();
 
+        // Once the loop is complete, close the scanner and exit the app.
         scanner.close();
     }
 }
--- src/main/java/com/thaleswell/bankapp/states/BankAppState.java
+++ src/main/java/com/thaleswell/bankapp/states/BankAppState.java
@@ -1,11 +1,21 @@
 package com.thaleswell.bankapp.states;
 
+import com.thaleswell.bankapp.services.banking.IBankService;
 import com.thaleswell.tui.io.IIO;
 import com.thaleswell.tui.states.BaseState;
 
 abstract public class BankAppState  extends BaseState{
+    static private IBankService bankService;
 
     public BankAppState(IIO io) {
         super(io);
+    }
+    
+    public static IBankService getBankService() {
+        return bankService;
+    }
+
+    public static void setBankService(IBankService bankService) {
+        BankAppState.bankService = bankService;
     }
 }
