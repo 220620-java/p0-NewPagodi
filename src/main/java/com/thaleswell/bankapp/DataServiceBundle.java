76e13a00849b504f24f347713350211a518293a0
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-04 14:52:46 -0500

    Add AccountService class and interface

--- src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
+++ src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
@@ -1,16 +1,24 @@
 package com.thaleswell.bankapp;
 
+import com.thaleswell.bankapp.services.data.AccountServicePostgres;
+import com.thaleswell.bankapp.services.data.IAccountService;
 import com.thaleswell.bankapp.services.data.IUserService;
 import com.thaleswell.bankapp.services.data.UserServicePostgres;
 
 public class DataServiceBundle {
     IUserService userService;
+    IAccountService accountService;
     
     public DataServiceBundle(){
         userService = new UserServicePostgres();
+        accountService = new AccountServicePostgres();
     }
     
     public IUserService getUserService() {
         return userService;
+    }
+    
+    public IAccountService getAccountService() {
+        return accountService;
     }
 }
