3d9c352bca9274f246fae3362e441fd12c56d3bc
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-03 23:36:55 -0500

    Create UserServicePostgres

---
+++ src/main/java/com/thaleswell/bankapp/services/data/UserServicePostgres.java
@@ -0,0 +1,27 @@
+package com.thaleswell.bankapp.services.data;
+
+import com.thaleswell.bankapp.data.UserDAO;
+import com.thaleswell.bankapp.data.UserPostgres;
+import com.thaleswell.bankapp.exceptions.UsernameAlreadyExistsException;
+import com.thaleswell.bankapp.models.User;
+
+public class UserServicePostgres implements IUserService{
+    private UserDAO userDao;
+    
+    public UserServicePostgres() {
+        userDao = new UserPostgres();
+    }
+
+    @Override
+    public User registerUser(String username, String password) throws UsernameAlreadyExistsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User logIn(String username, String password) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
