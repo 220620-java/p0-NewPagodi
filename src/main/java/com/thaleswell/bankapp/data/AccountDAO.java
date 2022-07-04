cb0d409dc27d3fb5b71bde69cd9a616047e78485
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-04 02:31:17 -0500

    Add AccountDAO interface and AccountPostgres class

---
+++ src/main/java/com/thaleswell/bankapp/data/AccountDAO.java
@@ -0,0 +1,8 @@
+package com.thaleswell.bankapp.data;
+
+import com.thaleswell.bankapp.ds.List;
+import com.thaleswell.bankapp.models.Account;
+
+public interface AccountDAO extends DataAccessObject<Account>{
+    public List<Account> findAllByUserId();
+}
---
+++ src/main/java/com/thaleswell/bankapp/data/AccountPostgres.java
@@ -0,0 +1,43 @@
+package com.thaleswell.bankapp.data;
+
+import com.thaleswell.bankapp.ds.List;
+import com.thaleswell.bankapp.models.Account;
+
+public class AccountPostgres implements AccountDAO{
+
+    @Override
+    public Account create(Account t) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Account findById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Account> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(Account t) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(Account t) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Account> findAllByUserId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
