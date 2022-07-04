0e9fc1ec305367d4f38ff8eaa9beee655e2dac98
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-03 23:36:21 -0500

    Create UserServicePostgres and use it in service bundle

--- src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
+++ src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
@@ -1,13 +1,13 @@
 package com.thaleswell.bankapp;
 
-import com.thaleswell.bankapp.services.data.DummyUserService;
 import com.thaleswell.bankapp.services.data.IUserService;
+import com.thaleswell.bankapp.services.data.UserServicePostgres;
 
 public class DataServiceBundle {
     IUserService userService;
     
     public DataServiceBundle(){
-        userService = new DummyUserService();
+        userService = new UserServicePostgres();
     }
     
     public IUserService getUserService() {
