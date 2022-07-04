4a20aaa2a0aea94fb693b298681b948d3c01886a
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-03 22:15:39 -0500

    Add List interface based on the petapp sample

---
+++ src/main/java/com/thaleswell/bankapp/ds/List.java
@@ -0,0 +1,34 @@
+package com.thaleswell.bankapp.ds;
+
+public interface List<T> {
+    /**
+     * Adds an element to the end of the list.
+     * 
+     * @param t the object to be added into the list
+     */
+    public void add(T t);
+    
+    /**
+     * Retrieves an element from a specified index.
+     * 
+     * @param index the index of the desired element
+     * @return the element at the specified index
+     */
+    public T get(int index);
+    
+    /**
+     * Removes the object at the specified index from the list and 
+     * returns the object.
+     * 
+     * @param index the index of the object to be removed
+     * @return the object that was removed
+     */
+    public T remove(int index);
+    
+    /**
+     * Returns the number of elements currently in the list.
+     * 
+     * @return the number of elements in the list
+     */
+    public int size();
+}
