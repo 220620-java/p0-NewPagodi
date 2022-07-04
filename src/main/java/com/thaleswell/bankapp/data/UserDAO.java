3123644137880bbf299185f6fa1e2dd1fc7cd103
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-03 22:53:25 -0500

    Add UserPostgress class empty methods

--- src/main/java/com/thaleswell/bankapp/data/UserDAO.java
+++ src/main/java/com/thaleswell/bankapp/data/UserDAO.java
@@ -2,7 +2,7 @@ package com.thaleswell.bankapp.data;
 
 import com.thaleswell.bankapp.models.User;
 
-public interface UserDAO {
+public interface UserDAO extends DataAccessObject<User> {
     public User findByUsername(String username);
 }
 
--- src/main/java/com/thaleswell/bankapp/data/UserPostgres.java
+++ src/main/java/com/thaleswell/bankapp/data/UserPostgres.java
@@ -1,5 +1,50 @@
 package com.thaleswell.bankapp.data;
 
-public class UserPostgres {
+import com.thaleswell.bankapp.ds.List;
+import com.thaleswell.bankapp.models.User;
+import com.thaleswell.bankapp.utils.ConnectionUtil;
 
+public class UserPostgres implements UserDAO {
+    
+    private ConnectionUtil connUtil;
+    
+    public UserPostgres() {
+        connUtil = ConnectionUtil.getConnectionUtil();
+    }
+
+    @Override
+    public User create(User t) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(User t) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(User t) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
