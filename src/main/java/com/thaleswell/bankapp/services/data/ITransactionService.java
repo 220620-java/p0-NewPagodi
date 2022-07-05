c687e8bcbc87b47f6f7a2a47eb454b5a882becc2
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-04 22:11:04 -0500

    Refine transaction serice classes

--- src/main/java/com/thaleswell/bankapp/services/data/ITransactionService.java
+++ src/main/java/com/thaleswell/bankapp/services/data/ITransactionService.java
@@ -1,5 +1,13 @@
 package com.thaleswell.bankapp.services.data;
 
+import com.thaleswell.bankapp.data.TransactionDAO;
+import com.thaleswell.bankapp.exceptions.TransactionOverdraftException;
+import com.thaleswell.bankapp.models.Account;
+import com.thaleswell.bankapp.models.Transaction;
+
 public interface ITransactionService {
 
+    Transaction deposit(Account account, double amount);
+    
+    Transaction withdraw(Account account, double amount) throws TransactionOverdraftException;
 }
--- src/main/java/com/thaleswell/bankapp/services/data/TransactionServicePostgres.java
+++ src/main/java/com/thaleswell/bankapp/services/data/TransactionServicePostgres.java
@@ -1,5 +1,29 @@
 package com.thaleswell.bankapp.services.data;
 
-public class TransactionServicePostgres {
+import com.thaleswell.bankapp.data.TransactionDAO;
+import com.thaleswell.bankapp.data.TransactionPostgres;
+import com.thaleswell.bankapp.exceptions.TransactionOverdraftException;
+import com.thaleswell.bankapp.models.Account;
+import com.thaleswell.bankapp.models.Transaction;
 
+public class TransactionServicePostgres implements ITransactionService{
+
+    private TransactionDAO transactionDAO;
+    
+    public TransactionServicePostgres() {
+        transactionDAO = new TransactionPostgres();
+    }
+    
+    @Override
+    public Transaction deposit(Account account, double amount) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Transaction withdraw(Account account, double amount) throws TransactionOverdraftException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
