0abadb47e97b2d570442dd99e4bfcfd18d9bc06a
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-04 23:04:54 -0500

    Add TransactionService to service bundle

--- src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
+++ src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
@@ -2,16 +2,20 @@ package com.thaleswell.bankapp;
 
 import com.thaleswell.bankapp.services.data.AccountServicePostgres;
 import com.thaleswell.bankapp.services.data.IAccountService;
+import com.thaleswell.bankapp.services.data.ITransactionService;
 import com.thaleswell.bankapp.services.data.IUserService;
+import com.thaleswell.bankapp.services.data.TransactionServicePostgres;
 import com.thaleswell.bankapp.services.data.UserServicePostgres;
 
 public class DataServiceBundle {
     IUserService userService;
     IAccountService accountService;
+    ITransactionService transactionService;
     
     public DataServiceBundle(){
         userService = new UserServicePostgres();
         accountService = new AccountServicePostgres();
+        transactionService = new TransactionServicePostgres();
     }
     
     public IUserService getUserService() {
@@ -20,5 +24,9 @@ public class DataServiceBundle {
     
     public IAccountService getAccountService() {
         return accountService;
+    }
+    
+    public ITransactionService getTransactionService( ) {
+        return transactionService;
     }
 }
