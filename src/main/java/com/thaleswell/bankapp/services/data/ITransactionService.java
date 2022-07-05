741319ba77577995e3cbde2d587067493867f82b
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-04 22:50:10 -0500

    Add date to TransactionService methods

--- src/main/java/com/thaleswell/bankapp/services/data/ITransactionService.java
+++ src/main/java/com/thaleswell/bankapp/services/data/ITransactionService.java
@@ -1,13 +1,15 @@
 package com.thaleswell.bankapp.services.data;
 
-import com.thaleswell.bankapp.data.TransactionDAO;
+import java.util.Date;
+
 import com.thaleswell.bankapp.exceptions.TransactionOverdraftException;
 import com.thaleswell.bankapp.models.Account;
 import com.thaleswell.bankapp.models.Transaction;
 
 public interface ITransactionService {
 
-    Transaction deposit(Account account, double amount);
+    Transaction deposit(Account account, Date date, double amount);
     
-    Transaction withdraw(Account account, double amount) throws TransactionOverdraftException;
+    Transaction withdraw(Account account, Date date, double amount)
+            throws TransactionOverdraftException;
 }
--- src/main/java/com/thaleswell/bankapp/services/data/TransactionServicePostgres.java
+++ src/main/java/com/thaleswell/bankapp/services/data/TransactionServicePostgres.java
@@ -1,5 +1,7 @@
 package com.thaleswell.bankapp.services.data;
 
+import java.util.Date;
+
 import com.thaleswell.bankapp.data.TransactionDAO;
 import com.thaleswell.bankapp.data.TransactionPostgres;
 import com.thaleswell.bankapp.exceptions.TransactionOverdraftException;
@@ -13,17 +15,17 @@ public class TransactionServicePostgres implements ITransactionService{
     public TransactionServicePostgres() {
         transactionDAO = new TransactionPostgres();
     }
-    
+
     @Override
-    public Transaction deposit(Account account, double amount) {
+    public Transaction deposit(Account account, Date date, double amount) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Transaction withdraw(Account account, double amount) throws TransactionOverdraftException {
+    public Transaction withdraw(Account account, Date date, double amount)
+            throws TransactionOverdraftException {
         // TODO Auto-generated method stub
         return null;
     }
-    
 }
