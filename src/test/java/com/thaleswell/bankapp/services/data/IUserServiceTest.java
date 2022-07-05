4f806edf39354ffb522a9581ce3295f104bd3f01
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-05 08:57:34 -0500

    Add skeleton for testing user service

---
+++ src/test/java/com/thaleswell/bankapp/services/data/IUserServiceTest.java
@@ -0,0 +1,17 @@
+package com.thaleswell.bankapp.services.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class IUserServiceTest {
+
+    @Test
+    void test() {
+        
+        System.out.println("======TEST ONE EXECUTED=======");
+        Assertions.assertTrue(true);
+    }
+
+}
