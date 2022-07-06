38bf5e23e5cfc18151bae56f64243d0c20b826d3
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-05 19:54:24 -0500

    Add skeleton test files for untested services

---
+++ src/test/java/com/thaleswell/bankapp/services/banking/IBankServiceTest.java
@@ -0,0 +1,14 @@
+package com.thaleswell.bankapp.services.banking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IBankServiceTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/java/com/thaleswell/bankapp/services/data/IAccountServiceTest.java
@@ -0,0 +1,14 @@
+package com.thaleswell.bankapp.services.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IAccountServiceTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
