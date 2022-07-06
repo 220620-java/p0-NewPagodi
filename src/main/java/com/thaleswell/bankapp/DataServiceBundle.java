066a52e0340aa7a6d9ee188fa4b62c5c9ae6ba92
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-06 00:36:59 -0500

    Add docs for DataServiceBundle

--- src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
+++ src/main/java/com/thaleswell/bankapp/DataServiceBundle.java
@@ -7,6 +7,11 @@ import com.thaleswell.bankapp.services.data.IUserService;
 import com.thaleswell.bankapp.services.data.TransactionServicePostgres;
 import com.thaleswell.bankapp.services.data.UserServicePostgres;
 
+/**
+ * Bundles up all of the data services into a single class.
+ * 
+ * @author michael
+ */
 public class DataServiceBundle {
     IUserService userService;
     IAccountService accountService;
@@ -18,14 +23,29 @@ public class DataServiceBundle {
         transactionService = new TransactionServicePostgres();
     }
     
+    /**
+     * Gets the user data service object.
+     * 
+     * @return The user service object.
+     */
     public IUserService getUserService() {
         return userService;
     }
     
+    /**
+     * Gets the bank account service object.
+     * 
+     * @return The account service object. 
+     */
     public IAccountService getAccountService() {
         return accountService;
     }
     
+    /**
+     * Gets the banking transaction data service object.
+     * 
+     * @return The transaction service object.
+     */
     public ITransactionService getTransactionService( ) {
         return transactionService;
     }
