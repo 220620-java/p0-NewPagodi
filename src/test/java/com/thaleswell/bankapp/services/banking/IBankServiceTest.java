b5fe02d16501d144d3153d92fde25a1eaa2ca149
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-05 23:16:06 -0500

    Add test cases for Bank Service

--- src/test/java/com/thaleswell/bankapp/services/banking/IBankServiceTest.java
+++ src/test/java/com/thaleswell/bankapp/services/banking/IBankServiceTest.java
@@ -1,14 +1,38 @@
 package com.thaleswell.bankapp.services.banking;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class IBankServiceTest {
+import com.thaleswell.tui.io.ConsoleIO;
+import com.thaleswell.tui.io.IIO;
 
+class IBankServiceTest {
+    
+    static Scanner scanner;
+    static IIO consoleIO;
+    static IBankService bankServ; 
+    
+    @BeforeAll
+    public static void setUp() {
+        System.out.println("======BEGIN IBankService Tests=======");
+        scanner = new Scanner(System.in);
+        consoleIO = new ConsoleIO(scanner);
+        bankServ = new DummyBankService(consoleIO);
+    }
+    
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testDispenseCash() {
+        assertDoesNotThrow(() -> bankServ.dispenseCash(1.00));
+    }
+    
+    @Test
+    void testAcceptDeposit() {
+        assertTrue(bankServ.acceptDeposit(1));
     }
 
 }
